3df494f8382555ef54923aed3bbc58dd7d5ee06e
michelle margan <michimargan@hotmail.com>
2023-05-05 17:19:17 -0300

    prueba

--- arturito/src/utils/fetcher.ts
+++ arturito/src/utils/fetcher.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-// Possible alternative: 
+
 const baseURL = 'https://swapi.dev/api';
 
 export const swGet = (url: string) =>
   axios.get(url, { baseURL }).then((res) => res.data);
 
-  //'https://www.swapi.it/api
+ 
